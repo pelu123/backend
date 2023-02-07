1b6721a951763798f837b837e3e1865451f8a2cd
pelu123 <tomascooklastra@gmail.com>
2023-02-06 23:34:13 -0300

    "segunda entrega"

---
+++ main.js
@@ -0,0 +1,33 @@
+
+class administradorDeProductos{
+
+    agregarInstrumento({id, instrumento, marca}) {
+        this.listado.push(new Instrumentos ({id, instrumento, marca}))
+    }
+
+    listaDeProductos() {
+        return this.listado
+    }
+
+    productoDisponible() {
+
+    }
+}
+
+class Instrumentos {
+    id
+    instrumento
+    marca
+
+    constructor ({id,instrumento, marca}){
+        this.id = id
+        this.instrumento = instrumento
+        this.marca = marca
+    }
+}
+
+const administradorDeProductos = new administradorDeProductos()
+
+administradorDeProductos.agregarInstrumento({id : '1' , instrumento : 'guitarra', marca : 'Fender'})
+console.log(administradorDeProductos.listaDeProductos())
+//managerDeProductos.productoDisponible(1)
